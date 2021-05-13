5429d3fa8ec963977f49e8af2904e2c6006dbac1
YS <yulia.deamchik@gmail.com>
2021-05-13 16:54:00 +0300

    replacing media

--- js/white_header.js
+++ js/white_header.js
@@ -2,14 +2,14 @@ $(window).on('scroll' , function()
 {
     if($('html').scrollTop() > 150)
     {
-        $('.link_menu').hover(function()
-        {
-            $(this).toggleClass('link_menu_white');
-        });
         $('.header_menu').css({'display' : 'none'});
         $('.header_menu_white').css({'display' : 'flex'});
         $('.logo').css({'display' : 'none'});
         $('.logo_black').css({'display' : 'flex'});
+        $('.link_menu_white').hover(function()
+        {
+            $(this).addClass('link_menu_white');
+        });
     }
     else
     {
